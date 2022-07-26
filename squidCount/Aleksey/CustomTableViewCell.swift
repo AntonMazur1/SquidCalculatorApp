97f78b48a01c5e7406d23475f46f58ace870eb7d
Amuzey <ivanovad97@gmail.com>
2022-07-26 22:39:17 +0500

    setting Custom ViewCell

---
+++ squidCount/Aleksey/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  squidCount
+//
+//  Created by Алексей on 26.07.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var avatarImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var positionLabel: UILabel!
+
+}
