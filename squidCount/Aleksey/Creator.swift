f268d54067d4468f998830346b2d8294e3b39118
Amuzey <ivanovad97@gmail.com>
2022-07-26 23:15:41 +0500

    setting VC

--- squidCount/Aleksey/Creator.swift
+++ squidCount/Aleksey/Creator.swift
@@ -9,16 +9,21 @@ import Foundation
 
 struct Creator {
     let name: String
+    let surname: String
     let position: String
     let photo: String
     
+    var fullName: String {
+        name + " " + surname
+    }
+    
     static func getEmployeDetails() -> [Creator] {
         [
-            Creator(name: "Антон", position: "TeamLead", photo: "Антон"),
-            Creator(name: "Илья", position: "Developer", photo: "Илья"),
-            Creator(name: "Владимир", position: "Developer", photo: "Владимир"),
-            Creator(name: "Сергей", position: "Developer", photo: "Сергей"),
-            Creator(name: "Алексей", position: "Developer", photo: "Алексей")
+            Creator(name: "Антон", surname: "Фамилия", position: "TeamLead", photo: "Антон"),
+            Creator(name: "Илья", surname: "Фамилия", position: "Developer", photo: "Илья"),
+            Creator(name: "Владимир", surname: "Прощин",  position: "Developer", photo: "Владимир"),
+            Creator(name: "Сергей", surname: "Фамилия", position: "Developer", photo: "Сергей"),
+            Creator(name: "Алексей", surname: "Иванов", position: "Developer", photo: "Алексей")
         ]
     }
 }
