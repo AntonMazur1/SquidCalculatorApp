ab17e868b2e8ef1f825c56373cd84b6389906969
Sergejs Tiselskis <slokas85@gmail.con>
2022-07-26 16:20:25 +0300

    finish

---
+++ squidCount/Figures.swift
@@ -0,0 +1,57 @@
+//
+//  Figures.swift
+//  squidCount
+//
+//  Created by John Doe on 26/07/2022.
+//
+
+import Foundation
+
+struct Rectangle {
+    
+    let width: Double
+    let lenght: Double
+    
+    var area: Double {
+        width * lenght
+    }
+    
+    var perimeter: Double {
+        2 * ( width + lenght)
+    }
+}
+
+struct Triangle {
+    
+    let base: Double
+    let height: Double
+    
+    var area: Double {
+        base * height / 2
+    }
+// расчитать периметр треугольника по основанию и высоте невозможно, я написал формулу для расчета периметра равностороннего треугольника.
+    
+    var perimeter: Double {
+        3 * base
+    }
+}
+
+struct Circle {
+    
+    let radius: Double
+    
+    var area: Double {
+        
+        Double.pi * pow(radius, 2)
+    }
+    
+    var perimeter: Double {
+        
+        2 * Double.pi * radius
+        
+    }
+}
+
+
+
+
