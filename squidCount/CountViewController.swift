a1228777237f84d1968579102839edda012a8e0e
Vladimir <vproshchin@gmail.com>
2022-07-27 14:09:34 +0300

    Setup all layouts

--- squidCount/CountViewController.swift
+++ squidCount/CountViewController.swift
@@ -8,24 +8,29 @@
 import UIKit
 
 class CountViewController: UIViewController {
-
+    
     @IBOutlet var figureImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
